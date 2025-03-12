591282cb70bcd65ac8b816ad98f851f5a07b3775
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-03-12 23:30:59 +0000

    update gameboard placeship method to require less input

--- src/javascript.test.js
+++ src/javascript.test.js
@@ -9,16 +9,25 @@ it('does is sunk work', () => {
 });
 
 it('does gameboard placing ship coors work', () => {
-  let testShip = new Ship(3);
-  let testGameboard = new GameBoard();
-  testGameboard.placeShip(testShip, ['00', '01', '02']);
-  expect(testShip.coords).toEqual(['00', '01', '02']);
+  let testGameboard = new GameBoard([new Ship(3), new Ship(7)]);
+  testGameboard.placeShip(testGameboard.ships[0], '00', 'y');
+  testGameboard.placeShip(testGameboard.ships[1], '10', 'x');
+  expect(testGameboard.ships[0].coords).toEqual(['00', '01', '02']);
+  expect(testGameboard.ships[1].coords).toEqual([
+    '10',
+    '20',
+    '30',
+    '40',
+    '50',
+    '60',
+    '70',
+  ]);
 });
 
 it('does recieveAttack work as intended', () => {
   let testGameboard = new GameBoard([new Ship(3), new Ship(1)]);
-  testGameboard.placeShip(testGameboard.ships[0], ['01', '02', '03']);
-  testGameboard.placeShip(testGameboard.ships[1], ['34']);
+  testGameboard.placeShip(testGameboard.ships[0], '01', 'y');
+  testGameboard.placeShip(testGameboard.ships[1], '34');
   testGameboard.receiveAttack('34');
   testGameboard.receiveAttack('02');
   testGameboard.receiveAttack('55');
@@ -28,22 +37,25 @@ it('does recieveAttack work as intended', () => {
 
 it('does gameboard record missed atatcks', () => {
   let testGameboard = new GameBoard([new Ship(2)]);
-  testGameboard.placeShip(testGameboard.ships[0], ['01', '02']);
+  testGameboard.placeShip(testGameboard.ships[0], '01', 'y');
   testGameboard.receiveAttack('03');
   expect(testGameboard.missedAttacks).toContain('03');
 });
 
 it('does allShipsSunk gameboard method work', () => {
   let testGameboard = new GameBoard([new Ship(2)]);
-  testGameboard.placeShip(testGameboard.ships[0], ['01', '02']);
+  testGameboard.placeShip(testGameboard.ships[0], '01', 'y');
+  console.log(testGameboard.ships[0].coords);
   testGameboard.receiveAttack('01');
+  console.log(testGameboard.ships[0].hits);
   testGameboard.receiveAttack('02');
+  console.log(testGameboard.ships[0].hits);
   expect(testGameboard.allShipsSunk()).toBeTruthy();
 });
 
 it('does Player class function as expected', () => {
   let playerOne = new Player('Mr.Blue Sky');
-  playerOne.gameboard.placeShip(playerOne.gameboard.ships[4], ['01', '02']);
+  playerOne.gameboard.placeShip(playerOne.gameboard.ships[4], '01', 'y');
   playerOne.gameboard.receiveAttack('01');
   expect(playerOne.gameboard.ships[4].isSunk()).toBeFalsy;
 });
